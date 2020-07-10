245ac4b66cd18e4c2d89c8c38bc2464a43d9b507
dhlee <dhlee@emro.co..kr>
2020-07-10 12:12:28 +0900

    master 에서 기본 webpack 적용 분리

--- README.md
+++ README.md
@@ -2,9 +2,10 @@ Webpack 테스트
 
 브랜치 List
 
-master 
-- Webpack 기본 - 번들링한 결과 자바스크립트 로딩
+master
 
-add-loader 
-- 로더 적용
-- css-loader, style-loader, file-loader, image-loader
+- 종합된 결과
+
+basic
+
+- Webpack 기본 - 번들링한 결과 자바스크립트 로딩
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require("path");
 
 // ES6 모듈(ESM) 시스템이 아니라 노드(Common.js) 모듈 시스템
 module.exports = {
-    mode: "development",
-    entry: {
-        main: "./src/app.js",
-    },
-    output: {
-        filename: "[name].js",
-        path: path.resolve("./dist"),
-    },
+  mode: "development",
+  entry: {
+    main: "./src/app.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve("./dist"),
+  },
 };
